7a474c8fe2e47eea2a9ee5c1006c509662efbcc3
Susie <susie@irons.email>
2021-01-25 14:47:32 +0000

    Algorithm working - testing required

--- Groups_algorithm.py
+++ Groups_algorithm.py
@@ -15,6 +15,8 @@ The algorithm should maximise the number of people each person meets over severa
 from random import choice, seed
 from math import ceil
 from timeit import timeit
+from collections import Counter
+import pandas as pd
 
 # Set seed for reproducibility
 set_seed = seed(42)
@@ -46,7 +48,6 @@ def assign_group(group_dict, group_n, student, student_list):
     """ A function that adds a student to the group and removes them from the main list"""
     group_dict[group_n].append(student)
     student_list.remove(student)
-    #    print("{} added to group".format(student))
     return group_dict, student_list
 
 
@@ -58,7 +59,6 @@ def remember_group(group_dict, group, memory):
                 memory[student_i].append(student_j)
     return memory
 
-
 ########################################################################################################################
 # Group builder function
 ########################################################################################################################
@@ -66,29 +66,21 @@ def remember_group(group_dict, group, memory):
 
 def form_group(group_dict, group_n, s_list, memory):
     # Create a loop that runs until the group size = 4
-    while len(group_dict[group_n]) <= 4:
+    for s in s_list:
         if len(group_dict[group_n]) == 4:
             break
-        # Randomly select a student from the copy list
-        chosen_student = choice(s_list)
-        #        print("Chosen student is {}".format(chosen_student))
 
-        # Check whether group is empty
+        # Check whether group is empty, if so, add to group
         if len(group_dict[group_n]) == 0:
-            # If so, add student to group
-            group_dict, s_list = assign_group(group_dict, group_n, chosen_student, s_list)
-        #                print(group_dict)
-        #                print(type(group_dict))
-        # Check whether student has  worked with an existing group member before
+            group_dict, s_list = assign_group(group_dict, group_n, s, s_list)
+        # Check whether student has  worked with an existing group member before, if so, skip step
         elif len([student for student in group_dict[group_n] if
-                  student in memory[chosen_student]]) > 0:
+                  student in memory[s]]) > 0:
             pass
         # If they have not worked with the chosen student, assign to a group
         else:
-            group_dict, s_list = assign_group(group_dict, group_n, chosen_student, s_list)
-    #                print(group_dict)
+            group_dict, s_list = assign_group(group_dict, group_n, s, s_list)
     return group_dict, s_list
-
 
 ########################################################################################################################
 # Populate n groups
@@ -99,8 +91,6 @@ def populate_groups(s_list, memory):
     # Calculate number of groups required
     copy_s_list = list(s_list)
     n_groups = ceil(len(copy_s_list) / 4)
-    #    print("{} groups required".format(n_groups))
-
     # Initialise group dictionary
     dictionary_of_groups = initialise_group_dict(n_groups)
 
@@ -137,7 +127,7 @@ def main(filename, iterations):
         dictionary_of_groups, s_memory = populate_groups(s_list, s_memory)
         # Add dictionary of groups to list of groups dictionary
         list_of_group_dicts.append(dictionary_of_groups)
-    return list_of_group_dicts
+    return list_of_group_dicts, s_memory
 
 
 ########################################################################################################################
@@ -145,11 +135,16 @@ def main(filename, iterations):
 ########################################################################################################################
 
 
-list_of_groups = main('hogwarts_students.csv', 2)
+list_of_groups, s_memory = main('hogwarts_students.csv', 3)
 print(list_of_groups[0])
 print(list_of_groups[1])
-#print(list_of_groups[2])
-#print(group_memory)
+print(list_of_groups[2])
+print(s_memory['Hermione Granger'])
+
+#list_of_groups_df = pd.DataFrame(data=list_of_groups[0])
+#print(list_of_groups_df)
+
+
 
 ########################################################################################################################
 # Time the functions
@@ -168,3 +163,54 @@ print(list_of_groups[1])
 
 #print("Three iterations took:")
 #print(timeit(three_it, number=1))
+
+########################################################################################################################
+# Test the functions
+########################################################################################################################
+
+# Check that each student is assigned only one group per iteration
+
+def check_gp_numbers(filename, iter):
+    # Run the function and return the results
+    list_of_groups, s_memory = main(filename, iter)
+    number_of_groups = len(list_of_groups[0].keys())
+
+    # For each iteration
+    for i in range(iter):
+        # Extract the values from each group
+        group_lists = [value for value in list_of_groups[i].values()]
+        # Add the values into a single list
+        student_names = list(group_lists[0])
+        for i in range(1, number_of_groups):
+            student_names = student_names + group_lists[i]
+        # Create a counter of each time a name occurs
+        group_count = Counter(student_names)
+        # Return any names where count > 1
+        errors = [value for value in group_count.values() if value > 1]
+    return group_count, errors
+
+
+#print(check_gp_numbers('hogwarts_students.csv', 40)[0])
+#print(check_gp_numbers('hogwarts_students.csv', 40)[1])
+
+# Check that no students are working with students they have previously worked with
+
+def cross_check_memory(student, filename, iter):
+    # Run the function and return the results
+    list_of_groups, s_memory = main(filename, iter)
+    print(list_of_groups)
+    # Create a groups list
+    groups = []
+    # Create a workmate list
+    workmates = []
+    # For each iteration
+    for i in range(1, iter):
+        for j in range(len(list_of_groups[i])):
+            for key, value in list_of_groups[i]:
+                return key, value
+        # Return the group the student was in
+        # Retun the students in that group
+        # Add to student list
+
+
+#print(cross_check_memory('Ron Weasley', 'hogwarts_students.csv', 2))
